8f54a1b6c1acfe9771ade945a12f9625c42a12e1
aditya <adityabikramkarki@gmail.com>
2022-07-30 13:54:38 +0545

    New Reddit Repository

---
+++ Scripts/index.js
@@ -0,0 +1,41 @@
+let myName = "Aditya";
+let myAge = 32;
+let a=1;
+let b=2;
+
+let names = ["Aditya","Kushal","Ram","Shyam"]
+const hobbies = ["Running","Football","Basketball","Swimming"]
+const grade = [4,5,6,7]
+
+
+const person1 = {
+    name:"Aditya",
+    age:32,
+    hobby:"running"
+
+}
+
+let person2 = {
+    name:"Kushal",
+    age:24,
+    hobby:"chess"
+}
+
+let people = [person1,person2];
+
+people[0].name="Ram";
+person2 = {
+    name:"Shyam"
+}
+
+
+console.log(myName);
+console.log(myAge);
+console.log(a+b);
+
+console.log(grade[0],grade[1]);
+console.log(names)
+console.log(hobbies[3])
+
+console.log(person2)
+
